98ca539148606500fb785afaa6b8f6fb48e04ac4
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-06-02 02:20:24 +0600

    add ex_1

--- ex_1/Program.cs
+++ ex_1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+System.Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if((num == 0) || (reverse(num, 0) == num) == true)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+    System.Console.WriteLine("No");
+}
+
+
+int reverse(int num, int reverse)
+{
+    while (num > 0)
+    { 
+        reverse = reverse * 10 + num % 10; 
+        num /= 10; 
+    }
+    return reverse;
+}
