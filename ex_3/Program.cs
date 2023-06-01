60bef75870fec2ebe4d908ec63db7c2a964f7f07
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-06-02 02:21:01 +0600

    add ex_3

---
+++ ex_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+void kub(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 0; j <= 3; j++)
+        {
+            count = i*i*i;
+        }
+        System.Console.Write($"{count} ");
+    }
+}
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+kub(num);
